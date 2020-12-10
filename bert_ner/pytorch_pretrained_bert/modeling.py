e6b82f78d4a2c860a24f76e86400d31454f9781e
shenfei1 <shenfei1@kingsoft.com>
2020-12-10 14:42:55 +0800

    init

--- bert_ner/pytorch_pretrained_bert/modeling.py
+++ bert_ner/pytorch_pretrained_bert/modeling.py
@@ -226,7 +226,7 @@ class BertConfig(object):
 
 try:
     from apex.normalization.fused_layer_norm import FusedLayerNorm as BertLayerNorm
-except ImportError:
+except (ImportError, ModuleNotFoundError):
     logger.info("Better speed can be achieved with apex installed from https://www.github.com/nvidia/apex .")
     class BertLayerNorm(nn.Module):
         def __init__(self, hidden_size, eps=1e-12):
